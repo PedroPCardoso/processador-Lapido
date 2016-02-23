ce05511111cf8e558eac768e5eeef24ea4ac8f6d
João Pedro Cardoso <pedroecomp@gmail.com>
2016-02-22 23:30:49 -0300

    ULA TB #semidone
    Falta o LSL e ASR

--- Verilog/ULA.v
+++ Verilog/ULA.v
@@ -27,23 +27,23 @@
         5'b00100:begin  Out = (A-B-32'b01); end // subdec
         5'b00101:begin  Out = (A-B);  end // Sub
         5'b00110:begin  Out = (A-32'b01); end // deca
-        5'b01000:begin  Out = (A+B); end  // lsl
-        5'b01001:begin  Out = (A+B); end // asr
+        5'b01000:begin  Out = (A+B); end  // lsl  //FAZER
+        5'b01001:begin  Out = (A+B); end // asr  //FAZER
         5'b10000:begin  Out = 32'b0; end // zeros
         5'b10001:begin  Out = (A&B); end// and
-        5'b10010:begin  Out = (~A&B); end // andnota
+        5'b10010:begin  Out = (~A)&B; end // andnota
         5'b10011:begin  Out = B; end // passb
-        5'b10100:begin  Out = (A&~B);end // andnotb
+        5'b10100:begin  Out = A&(~B);end // andnotb
         5'b10101:begin  Out = A;end // passa
         5'b10110:begin  Out = (A^B);end // xor
         5'b10111:begin  Out = (A|B);end // or
-        5'b11000:begin  Out = (~A&~B);end // nand
+        5'b11000:begin  Out = (~A)&(~B);end // nand
         5'b11001:begin  Out = ~(A^B); end // xnor
         5'b11010:begin  Out = ~A; end // passnota
         5'b11011:begin  Out = (~A)|B; end
-        5'b11100:begin  Out = ~B; end
+        5'b11100:begin  Out = ~B; end  // passnotb
         5'b11101:begin  Out = A|(~B); end
-        5'b11110:begin  Out = ~A|~B;end
+        5'b11110:begin  Out = (~A)|(~B);end
         5'b11111:begin  Out = 1;end //ones
         default:;
         endcase
