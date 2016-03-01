709ef54700f9e3b2cb4d1e8ef0407fb03201b61b
Weverson Gomes <weverson_santos@live.com>
2016-03-01 20:38:28 -0300

    Bios já lendo do arquivo

--- Verilog/bios.v
+++ Verilog/bios.v
@@ -7,7 +7,28 @@ reg CS;
 reg WE;
 reg OE;
 
+integer data_file    ; // file handler
+integer scan_file    ; // file handler
+reg [31:0] captured_data;
+`define NULL 0    
+
+	//always @(posedge clk) begin
+	always begin
+	  #10
+	  scan_file = $fscanf(data_file, "%b\n", captured_data); 
+	  if (!$feof(data_file)) begin
+		$display("Instruction: %b", captured_data);
+		//use captured_data as you would any other wire or reg value;
+	  end
+	end
+
 	initial begin
+		data_file = $fopen("instructions.dat", "r");
+		if (data_file == `NULL) begin
+			$display("data_file handle was NULL");
+			$finish;
+		end
+  
 		CS = 0;
 		OE = 1;
 		WE = 1;
@@ -24,7 +45,7 @@ reg OE;
 	assign Data = 32'b0110011;
 
 	//always @(posedge clock)
-	always 
+	always
 	begin
 		#100
 		WE=0;
