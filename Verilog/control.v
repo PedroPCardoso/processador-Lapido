19e7599cb7c7f4f46ebac416f0d099446cf5a478
Weverson Gomes <weverson_santos@live.com>
2016-03-22 22:50:45 -0300

    Gerenciamento dos sinais das emitidos pela UC para instruções que envolvem a ULA;

--- Verilog/control.v
+++ Verilog/control.v
@@ -42,6 +42,75 @@ output reg regWrite;
 			if(instruction[28] == 0 && instruction[27] == 0 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 0) begin
 				$display("Instrucao: Add");
 				ALUOp = 5'b00000;
+			end else if(instruction[28] == 0 && instruction[27] == 0 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 1) begin
+				$display("Instrucao: Addinc");
+				ALUOp = 5'b00001;
+			end else if(instruction[28] == 0 && instruction[27] == 0 && instruction[26] == 0 && instruction[25] == 1 && instruction[24] == 1) begin
+				$display("Instrucao: Inca");
+				ALUOp = 5'b00011;
+			end else if(instruction[28] == 0 && instruction[27] == 0 && instruction[26] == 1 && instruction[25] == 0 && instruction[24] == 0) begin
+				$display("Instrucao: Subdec");
+				ALUOp = 5'b00100;
+			end else if(instruction[28] == 0 && instruction[27] == 0 && instruction[26] == 1 && instruction[25] == 0 && instruction[24] == 1) begin
+				$display("Instrucao: Sub");
+				ALUOp = 5'b00101;
+			end else if(instruction[28] == 0 && instruction[27] == 0 && instruction[26] == 1 && instruction[25] == 1 && instruction[24] == 0) begin
+				$display("Instrucao: Deca");
+				ALUOp = 5'b00110;
+			end else if(instruction[28] == 0 && instruction[27] == 1 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 0) begin
+				$display("Instrucao: Lsl");
+				ALUOp = 5'b01000;
+			end else if(instruction[28] == 0 && instruction[27] == 1 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 1) begin
+				$display("Instrucao: Asr");
+				ALUOp = 5'b01001;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 0) begin
+				$display("Instrucao: Zeros");
+				ALUOp = 5'b10000;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 1) begin
+				$display("Instrucao: And");
+				ALUOp = 5'b10001;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 0 && instruction[25] == 1 && instruction[24] == 0) begin
+				$display("Instrucao: Andnota");
+				ALUOp = 5'b10010;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 0 && instruction[25] == 1 && instruction[24] == 1) begin
+				$display("Instrucao: Passb");
+				ALUOp = 5'b10011;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 1 && instruction[25] == 0 && instruction[24] == 0) begin
+				$display("Instrucao: Andnotb");
+				ALUOp = 5'b10100;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 1 && instruction[25] == 0 && instruction[24] == 1) begin
+				$display("Instrucao: Passa");
+				ALUOp = 5'b10101;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 1 && instruction[25] == 1 && instruction[24] == 0) begin
+				$display("Instrucao: Xor");
+				ALUOp = 5'b10110;
+			end else if(instruction[28] == 1 && instruction[27] == 0 && instruction[26] == 1 && instruction[25] == 1 && instruction[24] == 1) begin
+				$display("Instrucao: Or");
+				ALUOp = 5'b10111;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 0) begin
+				$display("Instrucao: Nand");
+				ALUOp = 5'b11000;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 0 && instruction[25] == 0 && instruction[24] == 1) begin
+				$display("Instrucao: Xnor");
+				ALUOp = 5'b11001;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 0 && instruction[25] == 1 && instruction[24] == 0) begin
+				$display("Instrucao: Passnota");
+				ALUOp = 5'b11010;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 0 && instruction[25] == 1 && instruction[24] == 1) begin
+				$display("Instrucao: Ornota");
+				ALUOp = 5'b11011;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 1 && instruction[25] == 0 && instruction[24] == 0) begin
+				$display("Instrucao: Passnotb");
+				ALUOp = 5'b11100;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 1 && instruction[25] == 0 && instruction[24] == 1) begin
+				$display("Instrucao: Ornotb");
+				ALUOp = 5'b11101;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 1 && instruction[25] == 1 && instruction[24] == 0) begin
+				$display("Instrucao: Nor");
+				ALUOp = 5'b11110;
+			end else if(instruction[28] == 1 && instruction[27] == 1 && instruction[26] == 1 && instruction[25] == 1 && instruction[24] == 1) begin
+				$display("Instrucao: Ones");
+				ALUOp = 5'b11111;
 			end
 		end
 		// Se for instrucao de memoria
