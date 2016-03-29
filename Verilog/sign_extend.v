98a5defa7e35152def817a2c3d597f265905137a
João Pedro Cardoso <pedroecomp@gmail.com>
2016-03-28 23:27:39 -0300

    Extensor de Sinal.  Diferenciando LCH e LCL.

--- Verilog/sign_extend.v
+++ Verilog/sign_extend.v
@@ -1,4 +1,4 @@
-module sign_extend(extend, extended);
+module sign_extend(clock,opcode , extend, extended);
 
 	input [15:0] extend;
 	output [31:0] extended;
@@ -6,8 +6,39 @@ module sign_extend(extend, extended);
 	wire [15:0] extend;
 	reg[31:0] extended;
 
-	always @ ( extend ) begin
-		extended[31:16] = 16'b0000000000000000 ;
-		extended[15:0] = extend;
-	end
+	always @ ( clock ) begin
+
+		if (opcode == 2'b11) begin
+					case ( extend[15] )
+					1'b0:begin
+							extended[31:16] = 16'b0000000000000000 ;
+							extended[15:0] = extend;
+					end
+					1'b1:begin
+							extended[31:16] = 16'b1111111111111111 ;
+							extended[15:0] = extend;
+					end
+					endcase
+			end
+
+
+		else begin
+
+							case ( extend[0] )
+							1'b0:begin
+									extended[31:16] =extend;
+									extended[15:0] =  16'b0000000000000000 ;
+							end
+							1'b1:begin
+									extended[31:16] =   extend;
+									extended[15:0] =16'b1111111111111111;
+							end
+
+							endcase
+
+
+		end
+
+
+		end
 endmodule
