90de2540d9612c7baf4f633ccea64a58297ce52b
Weverson Gomes <weverson_santos@live.com>
2016-03-30 15:39:00 -0300

    Extensor de sinal combinacional (não pode ser sequencial) com 3 modos de operação e respectivo testbench

--- Verilog/sign_extend.v
+++ Verilog/sign_extend.v
@@ -1,44 +1,22 @@
-module sign_extend(clock,opcode , extend, extended);
+module sign_extend(opcode, extend, extended);
 
+	input opcode;
 	input [15:0] extend;
 	output [31:0] extended;
 
-	wire [15:0] extend;
-	reg[31:0] extended;
+	wire [31:0] extendedWithSignal, extendedZeroOnLow, extendedZeroOnHigh;
 
-	always @ ( clock ) begin
+	assign extendedWithSignal[31:16] = (extend[15]) ? 16'b1111111111111111 : 16'b0 ;
+	assign extendedWithSignal[15:0] = extend;
 
-		if (opcode == 2'b11) begin
-					case ( extend[15] )
-					1'b0:begin
-							extended[31:16] = 16'b0000000000000000 ;
-							extended[15:0] = extend;
-					end
-					1'b1:begin
-							extended[31:16] = 16'b1111111111111111 ;
-							extended[15:0] = extend;
-					end
-					endcase
-			end
+	assign extendedZeroOnLow[31:16] = extend;
+	assign extendedZeroOnLow[15:0] = 16'b0;
 
+	assign extendedZeroOnHigh[31:16] = 16'b0;
+	assign extendedZeroOnHigh[15:0] = extend;
 
-		else begin
+	assign extended = (opcode == 2'b00) ? extendedWithSignal
+			: (opcode == 2'b01) ? extendedZeroOnLow
+			: extendedZeroOnHigh;
 
-							case ( extend[0] )
-							1'b0:begin
-									extended[31:16] =extend;
-									extended[15:0] =  16'b0000000000000000 ;
-							end
-							1'b1:begin
-									extended[31:16] =   extend;
-									extended[15:0] =16'b1111111111111111;
-							end
-
-							endcase
-
-
-		end
-
-
-		end
 endmodule
--- Verilog/sign_extend_tb.v
+++ Verilog/sign_extend_tb.v
@@ -1,24 +1,28 @@
 module sign_extend_tb();
-
+	reg[1:0] opcode;
 	reg[15:0] extend;
 	wire[31:0] extended;
 
-	sign_extend DUT (
+	sign_extend sign_extend(
+		.opcode(opcode),
 		.extend(extend),
 		.extended(extended)
 	);
 
 	initial begin
-				extend = 16'b0000111111111111;
-				#100;
-				$display("%b",extend);
-				$display("%b",extended);
-				extend = 16'b1110101010101010;
-				#100;
-				$display("%b",extend);
-				$display("%b",extended);
-
-
-		end
+		opcode = 2'b00;
+		extend = 16'b1111000000001111;
+		$display("Extend 15: %b", extend[15]);
+		#10;
+		opcode = 2'b00;
+		extend = 16'b0111000000001111;
+		$display("Extend 15: %b", extend[15]);
+		#10;
+		opcode = 2'b01;
+		extend = 16'b1111000000001111;
+		#10;
+		opcode = 2'b10;
+		extend = 16'b1111000000001111;
+	end
 
 endmodule
